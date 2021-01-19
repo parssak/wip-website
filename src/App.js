14d293d46012efe2e5c65b66c92693bc529b004b
parssa <parssak@gmail.com>
2021-01-19 14:30:32 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
-import './styles/App.scss';
+import { useState } from 'react';
 import Hero from './layout-components/Hero';
 import Projects from './layout-components/Projects';
 import NavBar from './layout-components/NavBar';
@@ -10,6 +9,10 @@ import Contact from './layout-components/Contact';
 import { useSelector } from 'react-redux';
 import focusToggler from './helper-functions/FocusToggler';
 import UserThemeListener from './helper-functions/UserThemeListener';
+
+import './styles/App.scss';
+import Footer from './minor-components/Footer';
+
 
 export default function App() {
   const theme = useSelector(state => state.theme);
@@ -25,24 +28,23 @@ export default function App() {
 
   return (
     <div className={`main ${theme}`}>
-
       <NavBar currFocus={currFocus} />
-      
+
       <div className="body">
-        <UserThemeListener/>
+        <UserThemeListener />
         <InView as="div" onChange={(inView, entry) => changeFocus('hero', inView)}>
           <Hero ref={heroRef} />
         </InView>
-        
+
         <InView as="div" onChange={(inView, entry) => changeFocus('projects', inView)}>
           <Projects ref={projectRef} />
         </InView>
 
         <InView as="div" onChange={(inView, entry) => changeFocus('contact', inView)}>
-          <Contact theme={theme}/>
+          <Contact theme={theme} />
         </InView>
-
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/minor-components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>This website was designed and built by me</p>
+            <p className="desktop-only">Thanks for stopping by!</p>
+        </div>
+    )
+}
